d7354ca82333328a30908b2e72cc4b254c34878b
Barrette Banner <barrettebanner@gmail.com>
2019-08-05 00:58:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,8 +25,10 @@ def long_planeteer_calls(array)# code an argument here
 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  array.find do |food|
+    cheese_types.include?(food)
+  end
 end
