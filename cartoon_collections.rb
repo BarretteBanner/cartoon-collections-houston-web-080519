a2016913641e05c965c68ce71ac3679cc8161fe4
Barrette Banner <barrettebanner@gmail.com>
2019-08-05 01:02:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ def summon_captain_planet(array)# code an argument here
   array.each do |word|
     newArray.push("#{word.capitalize}!")
   end
+  return newArray
 end
 
 def long_planeteer_calls(array)# code an argument here
